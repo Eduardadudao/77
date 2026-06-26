05935d03ff5942c3b968f7fc4260e8e479fb95df
Eduarda <Eduarda.oliveira.braz@escola.pr.gov.br>
2026-06-26 19:00:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-function resposta(correta) {
-  const resultado = document.getElementById("resultado");
+function responder(correto) {
+  let resultado = document.getElementById("resultado");
 
-  if (correta) {
-    resultado.innerHTML = "✔ Resposta correta! Agricultura de precisão reduz desperdícios.";
+  if (correto) {
+    resultado.innerHTML = "✅ Correto! Agricultura de precisão reduz desperdícios.";
     resultado.style.color = "#00ff88";
   } else {
-    resultado.innerHTML = "✘ Resposta incorreta. Tente novamente!";
+    resultado.innerHTML = "❌ Resposta incorreta. Tente novamente!";
     resultado.style.color = "red";
   }
+}
+
+function simular() {
+  let custo = Math.floor(Math.random() * 100) + 50;
+  let economia = Math.floor(custo * 0.35);
+
+  document.getElementById("saida").innerHTML =
+    `💡 Custo estimado: R$ ${custo} | Economia com tecnologia: R$ ${economia}`;
 }
